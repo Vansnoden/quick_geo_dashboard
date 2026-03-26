afb1b5b77b50646c7af13d015b9d12deabd5f173
salvador <atagong5@gmail.com>
2026-03-26 18:23:53 +0300

    update - code reorganized

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -7,8 +7,8 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { useActionState } from 'react';
-import { authenticate } from '@/app/lib/actions';
-import { Button } from './buttons';
+import { authenticate } from '@/lib/actions';
+import { Button } from '@/components/buttons';
 import Link from 'next/link';
 
 export default function LoginForm() {
--- frontend/components/nav-links.tsx
+++ frontend/components/nav-links.tsx
@@ -25,8 +25,8 @@ export default function NavLinks() {
                         key={link.name}
                         href={link.href}
                         className={clsx(
-                            'flex h-12 grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium 
-                            hover:bg-violet-100 hover:text-violet-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                            `flex h-12 grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium 
+                            hover:bg-violet-100 hover:text-violet-600 md:flex-none md:justify-start md:p-2 md:px-3`,
                             {
                                 'bg-violet-100 text-violet-600': pathname === link.href,
                             },
